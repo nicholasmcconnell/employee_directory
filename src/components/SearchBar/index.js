ed071510539e6f2d71e951e49ff65ac28090ebe4
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-04-08 20:35:03 -0400

    components and util folder added

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,18 @@
+import React from "./node_modules/react";
+import "./style.css";
+
+// Using the datalist element we can create autofill suggestions based on the props.breeds array
+function SearchBar(props) {
+  return (
+    // <form className="search">
+    //   <div className="form-group">
+    //     <label htmlFor="language">Search:</label>
+  
+    //   </div>
+    // </form>
+
+    <div>hi</div>
+  );
+}
+
+export default SearchBar;
