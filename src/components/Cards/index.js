ad006a602c373d30daf0c80987700686762af650
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-04-08 21:58:43 -0400

    basic styling started

---
+++ src/components/Cards/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "style.css";
+
+function Cards() {
+    return (
+        <div className="card">
+            <div className="img-container">
+
+            </div>
+            <div className="content">
+                <ul>
+                    <li>
+                        <strong>Name:</strong>
+                    </li>
+                    <li>
+                        <strong>Occupation:</strong>
+                    </li>
+                    <li>
+                        <strong>Location:</strong>
+                    </li>
+                </ul>
+            </div>
+            <span > Span</span>
+        </div>
+    );
+}
+
+export default Cards;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,12 +4,19 @@ import "./style.css";
 function Header() {
     return (
 
-       
-            <h1 className="display-4">Employee Directory</h1>
-           
-             
+        <div className="header">
+            <div className="jumbotron">
+                <h1 className="display-4">Employee Directory</h1>
+                <h1 className="display-4">Hello, world!</h1>
+                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
 
-       
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+
+
+            </div>
+
+        </div>
+
     );
 }
 
