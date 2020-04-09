e278e1d77f4903b4a8eb1efb4acf6d624c3021c3
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-04-08 22:46:49 -0400

    header.js search.js basic styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
+// import React, { useState, useEffect } from 'react';
 import React from 'react';
+
 // import logo from './logo.svg';
 // import './App.css';
+// import * as API from "./utils/API";
 import SearchBar from './components/SearchBar';
 import Header from './components/Header'
 
 function App() {
+  // const [developerState, setDeveloperState] = useState({
+   
+  // });
+
+  // useEffect(() => {
+  //   // For demonstration purposes, we mock an API call.
+  //   API.getDeveloper.then((res) => {
+  //     setDeveloperState(res);
+  //     console.log("Developer State:");
+  //     console.log(developerState);
+  //   });
+  // }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <Header />
         
-      <div><SearchBar /></div>
       </header>
         
+      <div><SearchBar /></div>
     </div>
   );
 }
---
+++ src/components/Employees/index.js
@@ -0,0 +1,2 @@
+import React from 'reacr';
+import 'style.css';
